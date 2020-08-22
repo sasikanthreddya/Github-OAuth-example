7dc82c65c504f3c002028273f0b70f37c12322c6
sasikanthreddy <40632736+sasikanthreddya@users.noreply.github.com>
2020-08-23 01:13:08 +0530

    Update EurekaServerRegistryApplication.java
    modifies

--- src/main/java/com/sasi/eureka/EurekaServerRegistryApplication.java
+++ src/main/java/com/sasi/eureka/EurekaServerRegistryApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EurekaServerRegistryApplication {
-
+ 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(EurekaServerRegistryApplication.class, args);
 	}
 
